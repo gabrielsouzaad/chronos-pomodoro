783704e2da6e9d6849a3244940f163ddb4bf6266
Gabriel Souza Dantas <gabrielsouzairru@gmail.com>
2026-02-09 11:17:20 -0300

    Variáveis e Reset de CSS do Chronos

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
-  console.log('Oi');
   return (
     <>
       <Heading attr={123} attr2= 'String' >Olá mundo 1</Heading> 
