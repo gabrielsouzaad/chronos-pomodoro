87b994de8efcbacfec770ee0d1945c96d27840cf
Gabriel Souza Dantas <gabrielsouzairru@gmail.com>
2026-02-21 01:39:26 -0300

    Criando a estrutura HTML e CSS do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
+
 
 export function App() {
   return (
     <>
-        <Heading>Olá mundo 1 
-        <button>
-         <TimerIcon /> 
-        </button>
-      
-        </Heading> 
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam qui
-        temporibus voluptate quae eos neque reiciendis modi harum dignissimos
-        hic et adipisci magni quidem, labore, eaque aperiam vel provident
-        exercitationem?
-      </p>
+     <div className="container">
+      <div className="content">
+        <section>LOGO</section>
+      </div>
+     </div>
+
+     <div className="container">
+      <div className="content">
+        <section>MENU</section>
+      </div>
+     </div>
+
+     <div className="container">
+      <div className="content">
+        <section>FORM</section>
+      </div>
+     </div>
+
+     <div className="container">
+      <div className="content">
+        <section>FOOTER</section>
+      </div>
+     </div>
     </>
   );
 }
