97d4ca9933291ff87564da86c13a97cf00599efa
Gabriel Souza Dantas <gabrielsouzairru@gmail.com>
2026-02-22 20:38:23 -0300

    Criando o formulário da página inicial

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,46 @@ import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 
-
 export function App() {
   return (
     <>
       <Container>
         <Logo />
-     </Container>
+      </Container>
 
-     <Container>
-        <Menu/>
-     </Container>
+      <Container>
+        <Menu />
+      </Container>
 
-     <Container>
-       <CountDown/>
-     </Container>
+      <Container>
+        <CountDown />
+      </Container>
 
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='meuInput'>task</label>
+            <input id='meuInput' type='text' />
+          </div>
+
+          <div className='formRow'>
+            <p>
+              Lorem ipsum dolor sit amet.
+            </p>
+          </div>
+
+          <div className='formRow'>
+            <p>
+              Ciclos
+            </p>
+            <p> 0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
-  
