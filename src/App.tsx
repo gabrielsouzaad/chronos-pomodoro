6c532d8edfe13853c3beda1f284c6a0f0a5ad9bd
Gabriel Souza Dantas <gabrielsouzairru@gmail.com>
2026-02-27 02:10:41 -0300

    Limpando o código + Tema do VSCode

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,13 @@ import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon, } from 'lucide-react';
 import { Footer } from './components/Footer';
-import { Heading } from './components/Heading';
-import { useState } from 'react';
+
+
 
 export function App() {
 
-  const [numero, setNumero] = useState(0);
-
-  function handleClick(){
-   setNumero((prevState) => prevState + 1);
-  }
   return (
     <>
-    <Heading>Numero: {numero}</Heading>
-    <button onClick={handleClick}>Aumenta</button>
       <Container>
         <Logo />
       </Container>
@@ -40,7 +33,7 @@ export function App() {
         <form className='form' action=''>
           <div className='formRow'>
             <DefaultInput
-              labelText={numero.toString()}
+              labelText='task'
               id='meuInput'
               type='text'
               placeholder='Digite algo'
