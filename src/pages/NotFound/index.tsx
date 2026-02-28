55bd9f760dde3b28a242b307b446768cf19a14e6
Gabriel Souza Dantas <gabrielsouzairru@gmail.com>
2026-02-28 17:35:46 -0300

    Criando a página Home e um template para todas as outras páginas

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,20 @@
+import { Container } from '../../components/Container';
+import { MainTemplate } from '../../Templates/MainTemplate';
+
+export function NotFound() {
+  return (
+    <MainTemplate>
+      <Container>
+
+        <h1>Página não encontrada</h1>
+        <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni harum
+        quibusdam accusantium culpa? Explicabo accusantium blanditiis eius
+        labore, dolores, nesciunt sed qui, iure amet est voluptates quia!
+        Accusamus, reprehenderit amet!
+        </p>
+
+      </Container>
+    </MainTemplate>
+  );
+}
