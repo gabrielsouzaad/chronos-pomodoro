70d73de6c1966f1987db252ccbd74d771972a6f3
Gabriel Souza Dantas <gabrielsouzairru@gmail.com>
2026-05-14 13:38:28 -0300

    Estado global da aplicação com useState e comunicação via props

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,25 @@ import { Home } from './pages/Home';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { useState } from 'react';
+import type { TaskStateModel } from './models/TaskStateModel';
 
+const initialSate: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+
+}
 export function App() {
+  const [state, setState] = useState(initialSate);
 
-  return <Home />
+  return <Home state={state} setState={setState} />
  
 }
